8f3fe37c48ad9f9f47a8835dc9862114c6e49c33
Migaku <migaku@example.com>
2023-10-05 01:01:41 +0900

    3リンクアーム検証

--- .ipynb_checkpoints/robot_arm_angles-checkpoint.py
+++ .ipynb_checkpoints/robot_arm_angles-checkpoint.py
@@ -10,32 +10,19 @@ OFFSET_Y = 10.0
 def computeIK_with_offset(x, y):
     y = y - OFFSET_Y  # Adjusting for the offset in y-coordinate
     d = math.sqrt(x**2 + y**2)
-    
-    cos_theta2 = (d**2 - L1**2 - L2**2) / (2 * L1 * L2)
-    theta2_rad1 = math.acos(cos_theta2)  # Elbow down solution
-    theta2_rad2 = -math.acos(cos_theta2)  # Elbow up solution
-    
+    theta2_rad = math.acos((d**2 - L1**2 - L2**2) / (2 * L1 * L2))
     alpha = math.atan2(y, x)
-    
-    beta1 = math.asin(L2 * math.sin(theta2_rad1) / d)
-    theta1_rad1 = alpha - beta1
-    
-    beta2 = math.asin(L2 * math.sin(theta2_rad2) / d)
-    theta1_rad2 = alpha - beta2
+    beta = math.asin(L2 * math.sin(theta2_rad) / d)
+    theta1_rad = alpha - beta
 
     # Convert from radians to degrees
-    theta1_deg1 = math.degrees(theta1_rad1) + 180
-    theta2_deg1 = math.degrees(theta2_rad1) + 180
-
-    theta1_deg2 = math.degrees(theta1_rad2) + 180
-    theta2_deg2 = math.degrees(theta2_rad2) + 180
-
-    return (theta1_deg1, theta2_deg1), (theta1_deg2, theta2_deg2)
-
+    theta1_deg = math.degrees(theta1_rad) + 180
+    theta2_deg = math.degrees(theta2_rad) + 180
+    return theta1_deg, theta2_deg
 
 def plot_robot_arm_with_offset(x, y):
     # Compute inverse kinematics
-    _, (theta1_deg, theta2_deg) = computeIK_with_offset(x, y)  # We only take the Elbow up solution
+    theta1_deg, theta2_deg = computeIK_with_offset(x, y)
     
     # Convert degrees to radians
     theta1_rad = np.radians(theta1_deg - 180)
@@ -54,6 +41,7 @@ def plot_robot_arm_with_offset(x, y):
     # Plot base
     plt.scatter(0, OFFSET_Y, color='red', s=100, label='Base')  # Adjusted for the offset
     
+
     # Plot link 1
     plt.plot([0, x1], [OFFSET_Y, y1], 'o-', color='blue', markersize=10, label='Link 1')  # Adjusted for the offset
     
@@ -74,8 +62,7 @@ def plot_robot_arm_with_offset(x, y):
     plt.show()
 
 # Plot robot arm for a sample position
-x_sample = 16
+x_sample = 10.5
 y_sample = 0
 # Plot robot arm for a sample position with the offset
 plot_robot_arm_with_offset(x_sample, y_sample)
-
